fd58a3893c80a0e45b1cea193ced7a18c313fd9a
Carlos <81259501+Carlou134@users.noreply.github.com>
2026-08-09 19:32:18 -0500

    feat: add global exception handling and structured logging with Serilog

--- SplunkSocWorker/Program.cs
+++ SplunkSocWorker/Program.cs
@@ -1,36 +1,59 @@
 using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Builder;
+using Serilog;
 using SplunkSocWorker.BackgroundServices;
 using SplunkSocWorker.Endpoints;
+using SplunkSocWorker.Middleware;
 using SplunkSocWorker.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/worker-.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 14)
+    .CreateLogger();
 
-builder.Services.AddSingleton<AlertsDatasetPool>();
-builder.Services.AddHostedService<BatchSenderWorker>();
-
-builder.Services.AddHttpClient<IHecClient, HecClient>((serviceProvider, client) =>
+try
 {
-    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-    var baseUrl = configuration["Hec:BaseUrl"] ?? "https://localhost:8088";
-    var token = configuration["Hec:Token"];
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
 
-    client.BaseAddress = new Uri(baseUrl.TrimEnd('/') + "/");
-    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Splunk", token);
-})
-.ConfigurePrimaryHttpMessageHandler(serviceProvider =>
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+    builder.Services.AddSingleton<AlertsDatasetPool>();
+    builder.Services.AddHostedService<BatchSenderWorker>();
+
+    builder.Services.AddHttpClient<IHecClient, HecClient>((serviceProvider, client) =>
+    {
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+        var baseUrl = configuration["Hec:BaseUrl"] ?? "https://localhost:8088";
+        var token = configuration["Hec:Token"];
+
+        client.BaseAddress = new Uri(baseUrl.TrimEnd('/') + "/");
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Splunk", token);
+    })
+    .ConfigurePrimaryHttpMessageHandler(serviceProvider =>
+    {
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+        var handler = new HttpClientHandler();
+
+        if (configuration.GetValue("Hec:IgnoreSslErrors", false))
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+
+        return handler;
+    });
+
+    var app = builder.Build();
+
+    app.UseExceptionHandler();
+    app.MapDatasetEndpoints();
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-    var handler = new HttpClientHandler();
-
-    if (configuration.GetValue("Hec:IgnoreSslErrors", false))
-        handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
-
-    return handler;
-});
-
-var app = builder.Build();
-
-app.MapDatasetEndpoints();
-
-app.Run();
+    Log.Fatal(ex, "El Worker termino de forma inesperada durante el arranque.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
